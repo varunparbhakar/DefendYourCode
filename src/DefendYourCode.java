861bbe6adb649c7beb729b7f401d2057f9600981
Rhanson7 <rhanson7@uw.edu>
2022-08-08 11:08:11 -0700

    Added a new branch

--- src/DefendYourCode.java
+++ src/DefendYourCode.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DefendYourCode {
-    public static void main(String[] args) {
-
+    
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		// Disregard this, team member is doing this
+		System.out.println("Please enter your name (LastName FirstName MI: ");
+		if (collectName(s.nextLine())) {
+			// Does something
+		}
+		// Password thing goes here
+		
     }
     /**
      * This method is used for testing a string with a regex.
@@ -26,7 +35,7 @@ public class DefendYourCode {
      * @return
      */
     public static boolean collectName(String theText) {
-        String regex = "[a-zA-Z]{2,20} [a-zA-Z]{2,20}( [a-zA-Z\\.]*)?";
+        String regex = "[a-zA-Z]{2,25} [a-zA-Z]{2,25}( [a-zA-Z\\.])?";
         return regexEngine(regex, theText);
     }
 }
